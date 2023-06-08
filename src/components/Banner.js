cc415b453b4c5645dd30248a1f53fa9e1d79476d
Ruslan Karpov <rkarpov88@gmail.com>
2023-06-08 19:42:38 +0300

    working with Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,72 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import Image from "../assets/avatar2.png";
+
+import { FaGithub, FaDribbble, FaLinkedin } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section className="section" id="home">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <h1 className="text-[42px] font-bold leading-[0.8] lg:text-[75px]">
+              Ruslan <span>Karpov</span>
+            </h1>
+            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+              <span className="mr-4">I am a</span>
+              <TypeAnimation
+                sequence={[
+                  "Developer",
+                  2000,
+                  "Engineer",
+                  2000,
+                  "Creator",
+                  2000,
+                  "Initiator",
+                  2000,
+                  "Designer",
+                  2000,
+                ]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </div>
+            <p className="mb-8 max-w-lg mx-auto lg:mx-0">
+              Hi there! I am a Full Stack developer looking for a full-time
+              position. I am responsible, practice and work with team for the
+              result are important for me.
+            </p>
+            <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My portfolio
+              </a>
+            </div>
+            <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+              <a href="#">
+                <FaLinkedin />
+              </a>
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaDribbble />
+              </a>
+            </div>
+          </div>
+          <div>
+            <img src={Image} alt="" className="rounded-full " />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
